27b8fb77da4693b17a914c5d76afce40a32099c6
Kristoffer Åberg <ka222vq@student.lnu.se>
2020-02-21 18:56:47 +0100

    moved functions reguarding storing images to its own file i middleware

--- api/routes/fishesRouter.js
+++ api/routes/fishesRouter.js
@@ -1,31 +1,6 @@
 const router = require('express').Router()
 const FishesController = require('../controllers/fishesController')
-const multer = require('multer')
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + file.originalname)
-    }
-})
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-        cb(null, true)
-    } else {
-        cb(null, false) // perhaps add some error handeling to let the user know that the image they try to upload is not valid
-    }
-}
-
-const upload = multer({ 
-    storage: storage,
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
-})
+const upload = require('../middleware/uploadImages')
 
 router
     .get('/', FishesController.get_all_fishes)
